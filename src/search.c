4ffd8c7adaf11c4f6c3b8957244e8e3e4e07aed2
wastegas <wastegas@gmail.com>
2014-04-27 22:48:24 -1000

    removed unused pos var and cleaned up formatting

--- src/search.c
+++ src/search.c
@@ -7,44 +7,50 @@
 
 void search() {
 
-    unsigned int id;
-    int found = 0;
-    fpos_t pos;
+    unsigned int    id;
+    int             found = 0;
+
+    Product* _p = malloc(sizeof(*_p));
+    if (_p == NULL) {
+
+        fprintf(stderr, "%s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+
+    }
+
+    FILE* fp = fopen("data.dat", "rb");
+    if (!fp) {
+
+        fprintf(stderr, "unable top open file for reading, %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+
+    }
 
     printf("Enter product id to display:");
     scanf("%d", &id);
 
-    Product* _p = malloc(sizeof(*_p));
-    if (_p == NULL) {
-        fprintf(stderr, "%s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
-    FILE* fp = fopen("data.dat", "rb");
-
-    if (!fp) {
-        fprintf(stderr, "unable top open file for reading, %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
     while (fread(_p, sizeof(Product), 1, fp)) {
+
         if (_p->pid == id) {
-            fgetpos(fp, &pos);
+
             found = 1;
             fclose(fp);
             break;
+
         }
+
     }
 
     if (found) {
 
       printone(_p);
 
+    } else {
+
+        printf("record was not found for editing\n");
+
     }
 
-    if (!found) {
-        printf("record was not found for editing\n");
-    }
     free(_p);
 
 }
