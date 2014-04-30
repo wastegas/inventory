7591694d22e874cd8076bb511e78c2f37613f0c7
wastegas <wastegas@gmail.com>
2014-04-29 21:10:09 -1000

    reformat to print one line per record

--- src/print.c
+++ src/print.c
@@ -17,12 +17,15 @@ void print() {
         exit(EXIT_FAILURE);
     }
 
+    printf("\n%3s\t%-15s\t%9s\t%9s\n","ID", "Name", "Price", "Qty");
     while ( fread(_p, sizeof(Product), 1, fp) != 0 ) {
-
+    /*
         printf("\nProduct ID   :\t%d\n", _p->pid);
         printf("Product Name :\t%s\n", _p->pname);
         printf("Product price:\t%4.2f\n", _p->price);
         printf("Product Qty  :\t%4.2f\n", _p->qty);
+     */
+        printf("%3d\t%-15s\t%9.2f\t%9.2f\n", _p->pid, _p->pname, _p->price, _p->qty);
     }
 
     fclose(fp);
