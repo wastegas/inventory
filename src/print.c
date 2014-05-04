e1ca6f1a5a3b4f53114bc0f3d12571246dbba34f
wastegas <wastegas@gmail.com>
2014-05-03 23:05:54 -1000

    use openFile function

--- src/print.c
+++ src/print.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <errno.h>
 #include "print.h"
+#include "openfile.h"
 
 void print() {
     Product* _p = malloc(sizeof(*_p));
@@ -11,21 +12,12 @@ void print() {
         exit(EXIT_FAILURE);
     }
 
-    FILE* fp = fopen("data.dat", "rb");
-    if(!fp) {
-        fprintf(stderr, "unable to open data.dat for reading %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
+    FILE* fp = openFile("data.dat", READING);
 
     printf("\n%3s\t%-15s\t%9s\t%9s\n","ID", "Name", "Price", "Qty");
     while ( fread(_p, sizeof(Product), 1, fp) != 0 ) {
-    /*
-        printf("\nProduct ID   :\t%d\n", _p->pid);
-        printf("Product Name :\t%s\n", _p->pname);
-        printf("Product price:\t%4.2f\n", _p->price);
-        printf("Product Qty  :\t%4.2f\n", _p->qty);
-     */
-        printf("%3d\t%-15s\t%9.2f\t%9.2f\n", _p->pid, _p->pname, _p->price, _p->qty);
+       printf("%3d\t%-15s\t%9.2f\t%9.2f\n", _p->pid, _p->pname, 
+               _p->price, _p->qty);
     }
 
     fclose(fp);
