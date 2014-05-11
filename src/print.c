010ecb990a817da2e33cce7196ca0da6ff5f6963
wastegas <wastegas@gmail.com>
2014-05-10 19:59:10 -1000

    add a total column

--- src/print.c
+++ src/print.c
@@ -14,10 +14,11 @@ void print() {
 
     FILE* fp = openFile("data.dat", READING);
 
-    printf("\n%3s\t%-15s\t%9s\t%9s\n","ID", "Name", "Price", "Qty");
+    printf("\n%3s\t%-15s\t%9s\t%9s\t%9s\n",
+            "ID", "Name", "Price", "Qty", "Total");
     while ( fread(_p, sizeof(Product), 1, fp) != 0 ) {
-       printf("%3d\t%-15s\t%9.2f\t%9.2f\n", _p->pid, _p->pname, 
-               _p->price, _p->qty);
+       printf("%3d\t%-15s\t%9.2f\t%9.2f\t%9.2f\n", _p->pid, _p->pname, 
+               _p->price, _p->qty, _p->price * _p->qty);
     }
 
     fclose(fp);
