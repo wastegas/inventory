337b67bfb762ccae15619e598e4e5753b7ca8464
wastegas <wastegas@gmail.com>
2014-08-04 21:07:36 -1000

    convert choice to uppercase

--- src/menu.c
+++ src/menu.c
@@ -1,36 +1,35 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <termios.h>
+#include <string.h>
+#include <errno.h>
 #include "menu.h"
 
 void menu() {
 
-    char choice;
+    int choice;
 
-    while ( choice != 'q' || choice != 'Q' ) {
-
+    while ( choice != 'Q' ) {
+    
         printf("\nA - Add | S = Search | E = Edit | D - Delete | P - Print | Q - Quit -> ");
         scanf(" %c", &choice);
-
-        switch (choice) {
-            case 'a':
+        switch (toupper(choice)) {
             case 'A':
                 add();
                 break;
-            case 's':
             case 'S':
                 search();
                 break;
-            case 'e':
             case 'E':
                 edit();
                 break;
-            case 'p':
             case 'P':
                 print();
                 break;
-            case 'd':
             case 'D':
                 delete();
                 break;
-            case 'q':
             case 'Q':
                 return;
             default:
