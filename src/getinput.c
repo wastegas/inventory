828382fbd51769c19c8424444ff140862039c1c5
wastegas <wastegas@gmail.com>
2014-08-06 07:35:14 -1000

    use getline for longer product names

--- src/getinput.c
+++ src/getinput.c
@@ -5,9 +5,20 @@
 
 char *getstr(char* str)
 {
-    char input[25];
-    scanf("%s", input);
+    //char input[25];
+    //scanf("%s", input);
+    size_t read_bytes = 0;
+    char *input;
+    size_t size = 25;
+    input = (char *)malloc(size + 1);
+    freopen("/dev/tty", "rw", stdin);
+    getline(&input, &size, stdin);
+    if (read_bytes == -1) {
+      puts("input error");
+      return NULL;
+    }
     strcpy(str, input);
+    free(input);
     return str;
 }
 
@@ -15,6 +26,7 @@ double *getdouble(double *input)
 {
     while (1) {
         if (scanf("%lf", input) == 1 ) {
+            getchar();
             return input;
         } else {
             printf("Invalid input...\n");
