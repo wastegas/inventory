b3af6cdbd65a7c24b8d31bc16e5446d4737300b8
wastegas <wastegas@gmail.com>
2014-08-11 08:11:40 -1000

    adjust spacing

--- src/print.c
+++ src/print.c
@@ -14,11 +14,12 @@ void print() {
 
     FILE* fp = openFile("data.dat", READING);
 
-    printf("\n%3s\t%-15s\t%9s\t%9s\t%9s\n",
+    printf("\n%3s\t%-10s\t%9s\t%9s\t%9s\n",
             "ID", "Name", "Price", "Qty", "Total");
     while ( fread(_p, sizeof(Product), 1, fp) != 0 ) {
-       printf("%3d\t%-15s\t%9.2f\t%9.2f\t%9.2f\n", _p->pid, _p->pname, 
-               _p->price, _p->qty, _p->price * _p->qty);
+       printf("%3d\t%-10s\t%9.2f\t%9.2f\t%9.2f\n", 
+              _p->pid, _p->pname,  _p->price, 
+              _p->qty, _p->price * _p->qty);
     }
 
     fclose(fp);
